636d6b0e8d831591bf502659f3be7b4ec17a03f6
rkimmi <kimmirindel97@gmail.com>
2018-04-12 13:52:08 +1200

    added error action+reducer

--- client/actions/users.js
+++ client/actions/users.js
@@ -1,6 +1,8 @@
 import request from 'superagent'
+import {showError} from './error'
 
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+
 
 export const receiveUsers = (users) => {
   return {
@@ -16,8 +18,8 @@ export function getUsers () {
       .then(res => {
         dispatch(receiveUsers(res.body.users))
       })
-      // .catch(() => {
-      //   dispatch(showError('An unexpected error in getting information'))
-      // })
+      .catch(() => {
+        dispatch(showError('An unexpected error in getting information'))
+      })
   }
 }
