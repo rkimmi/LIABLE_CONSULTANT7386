19be011e9b961981aa3d398c6ff9d6868b070e6b
Kimmi <kimmi@hmsnz.com>
2019-10-10 12:20:37 +1300

    Reformat to use Hooks

--- client/components/User.jsx
+++ client/components/User.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import request from 'superagent'
 import _ from 'lodash'
@@ -6,63 +6,52 @@ import _ from 'lodash'
 import usernameData from './username.json'
 import { getUsers } from '../actions/users'
 
-class User extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: '',
-        }
-        this.makeUsername = this.makeUsername.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+function handleSubmit(e, username, value) {
+  e.preventDefault()
+  request
+    .post('/api/v1/chatroom')
+    .send({
+      username,
+      value
+    })
+    .end()
+}
 
-    componentWillMount() {
-        this.makeUsername()
-        this.props.dispatch(getUsers())
-    }
+function User() {
+  const [value, setValue] = useState('')
+  const [username, setUser] = useState('')
 
-    handleSubmit(event) {
-        event.preventDefault()
-        request
-            .post('/api/v1/chatroom')
-            .send({
-                username: this.state.username
-            })
-            .end()
+  useEffect(function createUser() {
+    if (username === '' && value === '') {
+      const firstWord = _.sample(usernameData.first)
+      const secondWord = _.sample(usernameData.second)
+      const numeric = Number(_.random(1000, 10000))
+      const user = firstWord + '_' + secondWord + numeric
+      setUser(user)
+      const val = Number(_.random(1, 10))
+      setValue(val)
     }
+  })
 
-    makeUsername() {
-        const firstWord = _.sample(usernameData.first)
-        const secondWord = _.sample(usernameData.second)
-        const numeric = Number(_.random(1000, 10000))
-        const concatenate = firstWord + '_' + secondWord + numeric
-        this.setState({
-            username: concatenate
-        })
-    }
-
-
-    render() {
-        const user = this.props.users
-        return (
-            <div>
-                <form>
-                    <input type='hidden' value={this.state.username} />
-                    <button type='submit' onClick={(event) => this.handleSubmit(event)}>Enter chatroom</button>
-                    {/* {this.props.users && <h1> {this.props.users[0].username} </h1>}
-                    {this.props.users && <img src={this.props.users[1].icon} />} */}
-                </form>
-                <h1>{this.state.username}</h1>
-            </div>
-        )
-    }
+  return (
+    <div>
+      <form>
+        <input type='hidden' value={username} />
+        <button type='submit' onClick={(e) => handleSubmit(e, username, value)}>
+          Enter chatroom
+            </button>
+      </form>
+      <h1>{username}</h1>
+      <h1>{value}</h1>
+    </div>
+  )
 }
 
 
 const mapStateToProps = (state) => {
-    return {
-        users: state.users
-    }
+  return {
+    users: state.users
+  }
 }
 
 export default connect(mapStateToProps)(User)
