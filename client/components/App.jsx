6767078ad0be9ce0d5eeb67426ee74dbcf5b510c
Kimmi <kimmi@hmsnz.com>
2019-10-10 12:18:28 +1300

    Remove UserValue

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 import User from './User'
-import UserValue from './UserValue'
-
 
 const App = () => (
   <div className='app-container'>
     <User />
-    <UserValue />
   </div>
 )
 
