68dfd9e89d77b1fafe7fbd55f28a990025e6f48e
Kimmi <kimmi@hmsnz.com>
2019-10-10 12:19:29 +1300

    Add value property to user insert

--- server/db/db.js
+++ server/db/db.js
@@ -3,24 +3,24 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-    getUsers,
-    addUser
+  getUsers,
+  addUser
 }
 
-function getUsers () { // rename, gets everything
-    const conn = connection
-    return conn('users')
+function getUsers() { // rename, gets everything
+  const conn = connection
+  return conn('users')
     .join('messages', 'user_id', 'users.id')
     .select()
 }
 
-function addUser (userData) {
-    const conn = connection
-    return conn('users')
+function addUser(userData) {
+  const conn = connection
+  return conn('users')
     .insert({
-        username: userData.username
-        // value: req.body.value,
-        // icon: req.body.icon
-  })
+      username: userData.username,
+      value: userData.value,
+      // icon: req.body.icon
+    })
 }
 
